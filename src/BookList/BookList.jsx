944aaf37cbe5bf0bd97d1d869bc443eee9ba58af
Mai Huynh <nhumaihuynh91@gmail.com>
2018-12-17 12:41:50 -0600

    Book Search and Pagination: Not CSS yet

--- src/BookList/BookList.jsx
+++ src/BookList/BookList.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import * as axios from 'axios';
 import './BookList.css';
+import ReactPaginate from 'react-paginate';
 const url = 'http://localhost:3004/books'
 
 export class BookList extends Component{
@@ -8,12 +9,37 @@ export class BookList extends Component{
         super();
         this.state ={
             listView: true,
-            books: []
-        }
-        axios.get(url).then(response => {
-            this.setState({ books: response.data})
-        });
+            books: [],
+            pagination: {count:10, page:1, total:null},
+            searchItem: ""
+        };
+        this.loadBooks();
     }
+
+    loadBooks(){
+      let params = `?_page=${this.state.pagination.page}&_limit=${this.state.pagination.count}`;
+      if(this.state.searchItem) {params+=`&title_like=${this.state.searchItem}`};
+
+      axios.get(url+params).then(response => {
+        this.setState({
+           books: response.data,
+            pagination: {
+              ...this.state.pagination, 
+              total: +response.headers['x-total-count']
+         }
+         });
+    });
+    };
+
+    handleSearchInput = event => {
+      this.setState({ searchItem: event.target.value, pagination:{...this.state.pagination, page:1}}, this.loadBooks);
+    }
+
+    handlePageClick = reactPaginatePage => {
+      this.setState({pagination: {...this.state.pagination, page: reactPaginatePage.selected + 1}},
+      this.loadBooks )
+    };
+
 
     listView = () => {
         this.setState({listView: true})
@@ -84,8 +110,7 @@ export class BookList extends Component{
 
         return(
             <div>
-                <hr />
-                <hr />
+               
                 <div className="container">
           <div className="row">
             <h2>Our current bookstore</h2>
@@ -112,6 +137,41 @@ export class BookList extends Component{
               </div>
             </div>
           </div>
+
+          <div className='row'>
+          <ReactPaginate 
+              containerClassName="pagination pagination-lg"
+              breakClassName="page-item"
+              breakLabel={<span className="page-link">...</span>}
+              pageClassName="page-item"
+              activeClassName="active"
+              activeLinkClassName="disabled"
+              previousClassName="page-item"
+              nextClassName="page-item"
+              pageLinkClassName="page-link"
+              previousLinkClassName="page-link"
+              nextLinkClassName="page-link"
+              pageCount={
+                this.state.pagination.total / this.state.pagination.count
+              }
+              initialPage={this.state.pagination.page - 1}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={2}
+              onPageChange={this.handlePageClick}
+            />
+          </div>
+
+
+
+          <div>
+            <input 
+                type="text"
+                value={this.state.searchItem}
+                placeholder="Enter text"
+                onChange={this.handleSearchInput}
+              />
+          </div>
+
           <div id="products" className="row view-group">
             {renderedBookList}
           </div>
